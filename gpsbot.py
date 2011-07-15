dec0543b9d2b97530ec76f50a2f710ea05dd7290
Felix Sargent <felix.sargent@gmail.com>
2011-07-14 22:51:53 -0700

    Significantly built out GPSbot. Uses config files, able to daemonize.

--- gpsbot.py
+++ gpsbot.py
@@ -2,64 +2,102 @@
 import sys
 import tweepy
 import bitly_api
+import time
 
+# GPSbot by Felix Sargent (felix.sargent@gmail.com)
+#
+# GPSBot is designed to be used with twitter
+# when you want to let someone know where you are
+# just "@gpsbot @(friend) I'm here!"
+# and gpsbot will send them a google maps link to your location
 
 class Gpsbot(object):
-
+  
   def __init__(self):
     CONSUMER_KEY = 'b8tIIaVNf5uet7OFkqaf8w'
     CONSUMER_SECRET = '2bAcRYLQWp00At3HA1Knzm2JS3IXTs2fonlImGr0X8'
     ACCESS_KEY = '151351658-Vs6EQ2mXF2X3xipBxMA9X8LFyKo1BMmwgKCy7Lc2'
     ACCESS_SECRET = 'OnmccOtTZin91wrRaJk3Oe75Sw8gm7FFSdte96l92s'
-
+    
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     self.api = tweepy.API(auth)
     self.bitly = bitly_api.Connection("antagonist77", "R_d919ec73564a5b6f513c4f2d9fec0175")
-
-    
+    self.oldmentions = None
+  
+  
   def getUpdate(self):
     """Get gpsbot status"""
     self.mentions =  self.api.mentions()
 
+  def is_valid(self):
+    if self.mentions[0].geo != None:
+      return 1
+    else: return 0
+
+  def print_latest_tweet(self):
+    self.getUpdate()
+    return self.mentions[0].user.screen_name + ": " + self.mentions[0].text
+
+  def is_new(self):
+    if self.oldmentions[0].id_str != self.mentions[0].id_str:
+      return 1
+    else: return 0
+
+
   def makeMessage(self):
     """Take the latest update and turn it into a response string"""
-    try:
-      #grab the coordinates
-      mention =  self.mentions[0]
-      lat =  mention.place['bounding_box']['coordinates'][0][0][1]
-      lng =  mention.place['bounding_box']['coordinates'][0][0][0]
-      #grab the sender and the text, remove "@gpsbot"
-      sender = mention.user.screen_name
-      rawtext = mention.text
-      text = rawtext.replace("@gpsbot ","")
-      #create the URL and shorten it
-      url = 'http://maps.google.com/maps?q=%s+%s' % (lat,lng)
-      shortUrl = self.bitly.shorten(url)
-      self.response = "@"+ sender + " is here: " + shortUrl['url'] + " and says \"" + text + "\""
-      return self.response
-
-    except TypeError:
-      print "No GPS coordinates found."
+    mention = self.mentions[0]
+    #grab the coordinates
+    lat =  mention.geo['coordinates'][0]
+    lng =  mention.geo['coordinates'][1]
+    #grab the sender and the text, remove "@gpsbot"
+    sender = mention.user.screen_name
+    rawtext = mention.text
+    text_list = rawtext.split()
+    for w in text_list:
+      if w.lower() == "@gpsbot":
+        text_list.remove(w)
+    text = " ".join(text_list)
+    #create the URL and shorten it
+    url = 'http://maps.google.com/maps?q=%s+%s' % (lat,lng)
+    shortUrl = self.bitly.shorten(url)
+    self.response = "@"+ sender + " is here: " + shortUrl['url'] + " and says \"" + text + "\""
+    print self.response
+    return self.response
 
   def postResponse(self):
     self.api.update_status(self.response)
 
+  def daemonize(self):
+    while True:
+      print "Polling!"
+      self.poll()
+      self.oldmentions = self.mentions
+      time.sleep(60)
+      self.getUpdate()
+
+  def poll(self):
+    if self.is_valid() and self.is_new() == 1:
+      self.makeMessage()
+      print "Valid tweet, replying with:", self.response
+      self.postResponse()
+
+
   def main(self):
     choice = None
-   # self.getUpdate()
-   # self.makeMessage()
-   # print "Created: " + self.response
     while choice != "0":
         print \
-        """Twitter Message Sender
+        """GPSBOT - A Twitter Message Sender
 
         0 - Quit
         1 - Grab Updates
-        2 - Post Response
+        2 - Make Message
+        3 - Post Response
+        4 - Daemonize
         """
 
-        choice = raw_input("Choice:")
+        choice = raw_input("Choice: ")
         print
 
         if choice == "0":
@@ -67,19 +105,32 @@ class Gpsbot(object):
 
         elif choice == "1":
           self.getUpdate()
-          self.makeMessage()
-          print self.response
 
         elif choice == "2":
-          self.postResponse()
+          if self.is_valid == 1:
+            self.makeMessage()
+          else: print "No Geo Coordinates:\n\t", self.print_latest_tweet()
+
+        elif choice == "3":
+          try:
+            self.postReponse()
+            print "Responding with", self.response
+          except:
+            print "No response yet!"
+
+        elif choice == "4":
+          self.getUpdate()
+          self.oldmentions = self.mentions
+          self.daemonize()
+
+        elif choice == "5":
+          self.print_latest_tweet()
 
         #Unknown
         else:
           print "\nSorry, but", choice,"is not a valid choice."
 
 
-
-gpsbot = Gpsbot()
-gpsbot.main()
-raw_input("\n\nPress enter key to exit.")
-
+if __name__ == "__main__":
+  gpsbot = Gpsbot()
+  gpsbot.main()
